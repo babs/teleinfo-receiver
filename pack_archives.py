5f0808e36350a7a30686707787eed841869ab9bc
Damien Degois <damien@degois.info>
2014-12-24 03:23:59 +0100

    Added packing and merge existing archives

--- pack_archives.py
+++ pack_archives.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python
 
-import lzma, json, glob, sys
+import lzma, json, glob, sys, os
 from pprint import pprint
+from Colorize import init_root_logger
+
+log = init_root_logger()
+
+def archive_rank(rank, currank, currankfiles):
+    log.info( "Rank %d done, compressing..."%(rank))
+    archivename = 'archives/%d.json.xz'%rank
+    if os.path.exists(archivename):
+        try:
+            existingframes = json.load(lzma.LZMAFile(archivename, 'r'))
+            currank.update(existingframes)
+        except:
+            # Skip loop avoid rewrite
+            log.warning("Unable to load existing frames, not archiving.")
+            return
+
+    log.debug("compressing")
+    compressedvalue = lzma.compress(json.dumps(currank), options={'level':9, 'extreme': True})
+    log.debug("saving")
+    fp = open(archivename, 'w')
+    fp.write(compressedvalue)
+    fp.close()
+    log.debug("cleaning")
+    for framefile in currankfiles:
+        os.unlink(framefile)
+        #sys.exit(0)
+        #pass
+
 
 def main():
     trames = filter(lambda x: x != "dumps/.placeholder", glob.glob("dumps/*"))
@@ -9,6 +37,7 @@ def main():
 
     cnt = {}
     currank = {}
+    currankfiles = []
     prevrank = None
     donerank = []
 
@@ -16,15 +45,17 @@ def main():
         t = json.load(open(f, 'r'))
         rank = int(t['tramets'] / 50000)
         rank *= 50000
-        currank[t['tramets']] = t['trame']
+
+        # format float as string for json representation. %.6f
+        # if not, releading existing frames make double entries when serialized
+        currank["%.6f"%t['tramets']] = t['trame']
+        currankfiles.append(f)
         if prevrank != None and prevrank != rank and rank > prevrank:
+            archive_rank(rank, currank, currankfiles)
             donerank.append(prevrank)
-            print "Rank %d done, compressing..."%(rank)
-            fp = lzma.LZMAFile('archives/%d.json.xz'%rank, 'w', options={'level':9, 'extreme': True})
-            json.dump(currank, fp)
-            fp.close()
+            currank = {}
+            currankfiles = []
         prevrank = rank
-                
         
         if rank in cnt:
             cnt[rank] += 1
@@ -32,8 +63,11 @@ def main():
             cnt[rank] = 1
         #print rank
         #print "%.6f"%t['tramets']
-    pprint(cnt)
-    pprint(donerank)
+
+    for rank in cnt:
+        log.info("Rank %d: %d items"%(rank, cnt[rank]) )
+    if len(donerank) > 0:
+        log.info("Archived ranks: %s."%(", ".join(map(lambda x: str(x), donerank))) )
     
 
 if __name__ == "__main__":
