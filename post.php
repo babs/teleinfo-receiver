566388366fe8c00f62d0fb834ddea727761123e0
Damien Degois <damien@degois.info>
2014-12-24 04:08:28 +0100

    Return 500 if error while writing (make sender to queue records)

--- post.php
+++ post.php
@@ -1,5 +1,6 @@
 <?
 ini_set("precision", 16);
 $json = json_decode($GLOBALS["HTTP_RAW_POST_DATA"], true);
-file_put_contents('dumps/'.$json["tramets"], $GLOBALS["HTTP_RAW_POST_DATA"]);
-
+if (file_put_contents('dumps/'.$json["tramets"], $GLOBALS["HTTP_RAW_POST_DATA"]) === false) {
+   header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+}
